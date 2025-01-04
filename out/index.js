74a60aac4018fb181c8d4515b9f6ae8b5e4bf315
Brett R. Gebbie <brg078@gmail.com>
2025-01-03 22:35:21 -0600

    cleanup

--- out/index.js
+++ out/index.js
@@ -1,8 +1,5 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const smallestUnusedID_1 = require("./smallestUnusedID");
-//console.log('in index.js')
-console.log((0, smallestUnusedID_1.nextId)([0, 1, 2, 2, 3, 5]));
-console.log((0, smallestUnusedID_1.nextId)([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
-console.log((0, smallestUnusedID_1.nextId)([24, 2, 26, 20, 21, 19]));
+//console.log('in index.js');
+//import { nextId } from "./smallestUnusedID";
+console.log(());
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
+//console.log('in index.js');
 
-import { nextId } from "./smallestUnusedID";
-
-
-//console.log('in index.js')
+//import { nextId } from "./smallestUnusedID";
 
 
 
-console.log(nextId([0,1,2,2,3,5]));
-console.log(nextId([0,1,2,3,4,5,6,7,8,9,10]));
-console.log(nextId([24,2,26,20,21,19]));
+
+
+
+console.log(());
